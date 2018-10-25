8896de9ced2ac80e3dc8f7183a332585f9ed79c6
Ian LaFlamme <ian.laflamme@libertyits.com>
2018-10-25 15:18:52 -0400

    steelthread argonaut spring app

--- argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/impl/ArgonautV1ApiController.java
+++ argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/impl/ArgonautV1ApiController.java
@@ -1,5 +1,7 @@
 package gov.va.api.health.argonaut.service.controller.impl;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -9,25 +11,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
 @Slf4j
 public class ArgonautV1ApiController {
 
-    @RequestMapping(
-            value = {"/v1/hello"},
-            produces = {"application/json"},
-            method = RequestMethod.GET
-    )
-    @SneakyThrows
-    public Map<String, String> hello(ServerWebExchange exchange) {
-        Map<String, String> payload = new HashMap<>();
-        payload.put("message", "Hello World!");
-        return payload;
-    }
-
+  @RequestMapping(
+    value = {"/v1/hello"},
+    produces = {"application/json"},
+    method = RequestMethod.GET
+  )
+  @SneakyThrows
+  public Map<String, String> hello(ServerWebExchange exchange) {
+    Map<String, String> payload = new HashMap<>();
+    payload.put("message", "Hello World!");
+    return payload;
+  }
 }
