af5f4285439ad3d7c9a1b830545b28dcd2eaa0da
Bryan Schofield <bryan.schofield@libertyits.com>
2018-11-14 15:16:33 -0500

    Argonaut API updated service definition and swagger

--- argonaut-api/src/main/java/gov/va/api/health/argonaut/api/ArgonautService.java
+++ argonaut-api/src/main/java/gov/va/api/health/argonaut/api/ArgonautService.java
@@ -12,55 +12,71 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @OpenAPIDefinition(
-  info =
-      @Info(
-        title = "Argonaut",
-        version = "v1",
-        description =
-            "FHIR Argonaut implementation. See http://www.fhir.org/guides/argonaut/r2/index.html"
-      )
-)
+    info =
+        @Info(
+            title = "Argonaut",
+            version = "v1",
+            description =
+                "FHIR Argonaut implementation. See http://www.fhir.org/guides/argonaut/r2/index.html"))
 @Path("api")
 public interface ArgonautService {
 
   @Operation(
-    summary = "Patient read",
-    description = "http://www.fhir.org/guides/argonaut/r2/StructureDefinition-argo-patient.html"
-  )
+      summary = "Medication read",
+      description =
+          "http://www.fhir.org/guides/argonaut/r2/StructureDefinition-argo-medication.html")
+  @GET
+  @Path("Medication/{id}")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = Medication.class)))
+  @ApiResponse(responseCode = "400", description = "Not found")
+  @ApiResponse(responseCode = "404", description = "Bad request")
+  Medication medicationRead(
+      @Parameter(in = ParameterIn.PATH, name = "id", required = true) String id);
+
+  @Operation(
+      summary = "Patient read",
+      description = "http://www.fhir.org/guides/argonaut/r2/StructureDefinition-argo-patient.html")
   @GET
   @Path("Patient/{id}")
   @ApiResponse(
-    responseCode = "200",
-    description = "Record found",
-    content =
-        @Content(
-          mediaType = "application/fhir+json",
-          schema = @Schema(implementation = Patient.class)
-        )
-  )
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = Patient.class)))
   @ApiResponse(responseCode = "400", description = "Not found")
   @ApiResponse(responseCode = "404", description = "Bad request")
   Patient patientRead(@Parameter(in = ParameterIn.PATH, name = "id", required = true) String id);
 
   @Operation(
-    summary = "Medication read",
-    description = "http://www.fhir.org/guides/argonaut/r2/StructureDefinition-argo-medication.html"
-  )
+      summary = "Patient search",
+      description = "http://www.fhir.org/guides/argonaut/r2/StructureDefinition-argo-patient.html")
   @GET
-  @Path("Medication/{id}")
+  @Path("Patient")
   @ApiResponse(
-    responseCode = "200",
-    description = "Record found",
-    content =
-        @Content(
-          mediaType = "application/fhir+json",
-          schema = @Schema(implementation = Medication.class)
-        )
-  )
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = Patient.Bundle.class)))
   @ApiResponse(responseCode = "400", description = "Not found")
   @ApiResponse(responseCode = "404", description = "Bad request")
-  Medication medicationRead(
-      @Parameter(in = ParameterIn.PATH, name = "id", required = true) String id);
+  Patient.Bundle patientSearch(
+      @Parameter(in = ParameterIn.QUERY, name = "_id") String id,
+      @Parameter(in = ParameterIn.QUERY, name = "identifier") String identifier,
+      @Parameter(in = ParameterIn.QUERY, name = "name") String name,
+      @Parameter(in = ParameterIn.QUERY, name = "given") String given,
+      @Parameter(in = ParameterIn.QUERY, name = "family") String family,
+      @Parameter(in = ParameterIn.QUERY, name = "gender") String gender,
+      @Parameter(in = ParameterIn.QUERY, name = "birthdate") String[] birthdate);
 
   class ArgonautServiceException extends RuntimeException {
     ArgonautServiceException(String message) {
@@ -68,15 +84,15 @@ public interface ArgonautService {
     }
   }
 
-  class UnknownResource extends ArgonautServiceException {
-    public UnknownResource(String id) {
-      super(id);
-    }
-  }
-
   class SearchFailed extends ArgonautServiceException {
     public SearchFailed(String id, String reason) {
       super(id + " Reason: " + reason);
+    }
+  }
+
+  class UnknownResource extends ArgonautServiceException {
+    public UnknownResource(String id) {
+      super(id);
     }
   }
 }
