f375b51f4991b439e119a728889a6f955079715c
Ian LaFlamme <ian.laflamme@libertyits.com>
2018-11-14 15:41:19 -0500

    Added FHIR media type support to sentinal

--- sentinel/src/main/java/gov/va/health/api/sentinel/FhirTestClient.java
+++ sentinel/src/main/java/gov/va/health/api/sentinel/FhirTestClient.java
@@ -1,18 +1,20 @@
 package gov.va.health.api.sentinel;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.va.api.health.argonaut.api.OperationOutcome;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import java.util.function.Supplier;
 import javax.ws.rs.NotSupportedException;
 import lombok.Builder;
 import lombok.Value;
-import static org.assertj.core.api.Assertions.assertThat;
-
-
+import lombok.extern.slf4j.Slf4j;
 
 @Value
 @Builder
+@Slf4j
 public class FhirTestClient implements TestClient {
   private final ServiceDefinition service;
   Supplier<ObjectMapper> mapper;
@@ -25,17 +27,32 @@ public class FhirTestClient implements TestClient {
             .contentType("application/json")
             .request()
             .request(Method.GET, path, (Object[]) params);
-    Response fhirJsonResponse = service()
+    Response fhirJsonResponse =
+        service()
             .requestSpecification()
             .contentType("application/fhir+json")
             .request()
             .request(Method.GET, path, (Object[]) params);
-    Response jsonFhirResponse = service()
+    Response jsonFhirResponse =
+        service()
             .requestSpecification()
             .contentType("application/json+fhir")
             .request()
             .request(Method.GET, path, (Object[]) params);
-    assertThat(baselineResponse).withFailMessage("Fhir media types return inconsistent responses.").isEqualTo(fhirJsonResponse).isEqualTo(jsonFhirResponse);
+    final int valid = 200;
+    if (baselineResponse.statusCode() == valid
+        && fhirJsonResponse.statusCode() == valid
+        && jsonFhirResponse.statusCode() == valid) {
+      assertThat(baselineResponse.body().asString())
+          .withFailMessage("Fhir Media Types are getting inconsistent results.")
+          .isEqualTo(fhirJsonResponse.body().asString())
+          .isEqualTo(jsonFhirResponse.body().asString());
+    } else {
+      assertThat(baselineResponse.body().as(OperationOutcome.class).text())
+          .withFailMessage("Fhir Media Types are getting inconsistent results.")
+          .isEqualTo(fhirJsonResponse.body().as(OperationOutcome.class).text())
+          .isEqualTo(jsonFhirResponse.body().as(OperationOutcome.class).text());
+    }
     return ExpectedResponse.of(baselineResponse);
   }
 
@@ -43,6 +60,4 @@ public class FhirTestClient implements TestClient {
   public ExpectedResponse post(String path, Object body) {
     throw new NotSupportedException();
   }
-
-
 }
