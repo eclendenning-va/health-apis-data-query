c4208a44a7dea6eee78021ca4e5fb71dc3b51606
Bryan Schofield <bryan.schofield@libertyits.com>
2018-11-14 16:46:53 -0500

    Argonaut - Patient controller now accepts fhir media types
    Sentinel
    - FhirTestCient now explicitly compares the status codes to each other and
      performs a deeper OO comparison

--- sentinel/src/main/java/gov/va/health/api/sentinel/FhirTestClient.java
+++ sentinel/src/main/java/gov/va/health/api/sentinel/FhirTestClient.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.api.health.argonaut.api.OperationOutcome;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import java.util.function.Supplier;
 import javax.ws.rs.NotSupportedException;
 import lombok.Builder;
@@ -19,41 +20,65 @@ public class FhirTestClient implements TestClient {
   private final ServiceDefinition service;
   Supplier<ObjectMapper> mapper;
 
+  /**
+   * Remove data from the OO that is unique for each instance. This includes the generated ID and
+   * the timestamp.
+   */
+  private OperationOutcome asOperationOutcomeWithoutDiagnostics(ResponseBody body) {
+    OperationOutcome oo = body.as(OperationOutcome.class);
+    oo.id("REMOVED-FOR-COMPARISON");
+    oo.issue()
+        .forEach(
+            i -> {
+              if (i.diagnostics() != null) {
+                i.diagnostics(
+                    i.diagnostics()
+                        .replaceAll("Timestamp:.*$", "Timestamp:REMOVED-FOR-COMPARISON"));
+              }
+            });
+    return oo;
+  }
+
   @Override
   public ExpectedResponse get(String path, String... params) {
-    Response baselineResponse =
-        service()
-            .requestSpecification()
-            .contentType("application/json")
-            .request()
-            .request(Method.GET, path, (Object[]) params);
-    Response fhirJsonResponse =
-        service()
-            .requestSpecification()
-            .contentType("application/fhir+json")
-            .request()
-            .request(Method.GET, path, (Object[]) params);
-    Response jsonFhirResponse =
-        service()
-            .requestSpecification()
-            .contentType("application/json+fhir")
-            .request()
-            .request(Method.GET, path, (Object[]) params);
-    final int valid = 200;
-    if (baselineResponse.statusCode() == valid
-        && fhirJsonResponse.statusCode() == valid
-        && jsonFhirResponse.statusCode() == valid) {
+    Response baselineResponse = get("application/json", path, params);
+    Response fhirJsonResponse = get("application/fhir+json", path, params);
+    Response jsonFhirResponse = get("application/json+fhir", path, params);
+
+    assertThat(fhirJsonResponse.getStatusCode())
+        .withFailMessage("status: application/json and application/fhir+json")
+        .isEqualTo(baselineResponse.getStatusCode());
+    assertThat(jsonFhirResponse.getStatusCode())
+        .withFailMessage("status: application/json and application/json+fhir")
+        .isEqualTo(baselineResponse.getStatusCode());
+
+    if (baselineResponse.getStatusCode() >= 400) {
+      /*
+       * Error responses must be returned as OOs but contain a timestamp in the diagnostics
+       * that prevents direct comparison.
+       */
+      assertThat(asOperationOutcomeWithoutDiagnostics(baselineResponse.body()))
+          .isEqualTo(asOperationOutcomeWithoutDiagnostics(fhirJsonResponse.body()))
+          .isEqualTo(asOperationOutcomeWithoutDiagnostics(jsonFhirResponse.body()));
+      ;
+    } else {
+      /*
+       * OK responses
+       */
       assertThat(baselineResponse.body().asString())
-          .withFailMessage("Fhir Media Types are getting inconsistent results.")
           .isEqualTo(fhirJsonResponse.body().asString())
           .isEqualTo(jsonFhirResponse.body().asString());
-    } else {
-      assertThat(baselineResponse.body().as(OperationOutcome.class).text())
-          .withFailMessage("Fhir Media Types are getting inconsistent results.")
-          .isEqualTo(fhirJsonResponse.body().as(OperationOutcome.class).text())
-          .isEqualTo(jsonFhirResponse.body().as(OperationOutcome.class).text());
     }
+
     return ExpectedResponse.of(baselineResponse);
+  }
+
+  private Response get(String contentType, String path, Object[] params) {
+    return service()
+        .requestSpecification()
+        .contentType(contentType)
+        .request()
+        .request(Method.GET, path, params);
   }
 
   @Override
