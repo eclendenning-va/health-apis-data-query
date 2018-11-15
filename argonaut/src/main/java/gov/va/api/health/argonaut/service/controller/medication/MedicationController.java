08615d6a578d9ff9d5b7fbe9ca39ba4d818b54a8
Bryan Schofield <bryan.schofield@libertyits.com>
2018-11-15 13:27:21 -0500

    Argonaut - updated media types for Medication

--- argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/medication/MedicationController.java
+++ argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/medication/MedicationController.java
@@ -25,9 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(
-  value = {"/api/Medication"},
-  produces = {"application/json"}
-)
+    value = {"/api/Medication"},
+    produces = {"application/json", "application/json+fhir", "application/fhir+json"})
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
 @Slf4j
 public class MedicationController {
