ff1f592ce8fb7874468de12da0394d487ce662db
jjennings <joshua.jennings@libertyits.com>
2018-11-15 16:55:30 -0500

    Added in health check tests and general clean up

--- argonaut/src/main/java/gov/va/api/health/argonaut/service/healthcheck/SteelThreadSystemCheck.java
+++ argonaut/src/main/java/gov/va/api/health/argonaut/service/healthcheck/SteelThreadSystemCheck.java
@@ -1,51 +1,49 @@
 package gov.va.api.health.argonaut.service.healthcheck;
 
 import gov.va.api.health.argonaut.service.controller.Parameters;
+import gov.va.api.health.argonaut.service.mranderson.client.MrAndersonClient;
 import gov.va.api.health.argonaut.service.mranderson.client.Query;
-import gov.va.api.health.argonaut.service.mranderson.client.RestMrAndersonClient;
-import gov.va.dvp.cdw.xsd.pojos.Patient103Root;
+import gov.va.dvp.cdw.xsd.model.CdwMedication101Root;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
-import javax.xml.bind.annotation.XmlRootElement;
-
-
 
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
 @Component
-
+@Slf4j
 public class SteelThreadSystemCheck implements HealthIndicator {
 
-  RestMrAndersonClient client;
+  private final MrAndersonClient client;
 
-  @Value("${health.resource}")
-  String resource;
-  @Value("${health.version}")
-  String version;
   @Value("${health.id}")
-  String id;
+  private final String id;
 
   @Override
+  @SneakyThrows
   public Health health() {
     try {
       client.search(query());
       return Health.up().withDetail("Status", "JArgonaut, MR-Anderson, IDS, and DB Stored Procedure all working as expected").build();
+    } catch (MrAndersonClient.MrAndersonServiceException e) {
+      return Health.down().withDetail("Error", e).build();
     } catch (Exception e) {
-      return Health.down().withDetail("Error", client.search(query())).build();
+      log.error("Failed to complete health check.",e);
+      throw e;
     }
+
   }
 
-  private Query<PatientSearchResultsRoot> query() {
-    return Query.forType(PatientSearchResultsRoot.class)
-        .resource(resource)
+  private Query<CdwMedication101Root> query() {
+    return Query.forType(CdwMedication101Root.class)
+        .resource("Medication")
         .profile(Query.Profile.ARGONAUT)
-        .version(version)
+        .version("1.01")
         .parameters(Parameters.forIdentity(id))
         .build();
   }
-  @XmlRootElement(name = "root")
-  public static class PatientSearchResultsRoot extends Patient103Root {}
 }
