bb990a7cfe7c505185280c367742de6b0f950c2e
Ian LaFlamme <ian.laflamme@libertyits.com>
2018-11-20 10:29:44 -0500

    WIP; DR transformer

--- argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/diagnosticreport/DiagnosticReportTransformer.java
+++ argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/diagnosticreport/DiagnosticReportTransformer.java
@@ -1,8 +1,17 @@
 package gov.va.api.health.argonaut.service.controller.diagnosticreport;
 
+import gov.va.api.health.argonaut.api.datatypes.CodeableConcept;
+import gov.va.api.health.argonaut.api.datatypes.Coding;
 import gov.va.api.health.argonaut.api.resources.DiagnosticReport;
+import gov.va.api.health.argonaut.service.controller.Parameters;
+import gov.va.dvp.cdw.xsd.model.*;
 import gov.va.dvp.cdw.xsd.model.CdwDiagnosticReport102Root.CdwDiagnosticReports.CdwDiagnosticReport;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static gov.va.api.health.argonaut.service.controller.Transformers.convertAll;
+import static gov.va.api.health.argonaut.service.controller.Transformers.ifPresent;
 
 @Service
 public class DiagnosticReportTransformer implements DiagnosticReportController.Transformer {
@@ -16,6 +25,34 @@ public class DiagnosticReportTransformer implements DiagnosticReportController.T
         return DiagnosticReport.builder()
                 .id(source.getCdwId())
                 .resourceType("Diagnostic Report")
+                .status(status(source))
+                .category(category(source.getCategory()))
+                .code(code(source.getCode()))
                 .build();
+    }
+
+    //TODO: Stopping here for now
+    private CodeableConcept code(CdwDiagnosticReportCode source) {
+        return null;
+    }
+
+    private CodeableConcept category(CdwDiagnosticReportCategory source) {
+        return CodeableConcept.builder()
+                .coding(categoryCodings(source.getCoding()))
+                .text(source.getText())
+                .build();
+    }
+
+    private List<Coding> categoryCodings(List<CdwDiagnosticReportCategoryCoding> optionalSource) {
+        return convertAll(
+                optionalSource,
+                cdw -> Coding.builder()
+                        .system(cdw.getSystem())
+                        .code(ifPresent(cdw.getCode(), CdwDiagnosticReportCategoryCode::value))
+                        .build());
+    }
+
+    private DiagnosticReport.Code status(CdwDiagnosticReport source) {
+        return ifPresent(source.getStatus(), status -> DiagnosticReport.Code.valueOf(status.value()));
     }
 }
--- argonaut/src/test/java/gov/va/api/health/argonaut/service/controller/diagnosticreport/DiagnosticReportTransformerTest.java
+++ argonaut/src/test/java/gov/va/api/health/argonaut/service/controller/diagnosticreport/DiagnosticReportTransformerTest.java
@@ -1,3 +1,22 @@
 package gov.va.api.health.argonaut.service.controller.diagnosticreport;
 
-public class DiagnosticReportTransformerTest {}
+import gov.va.api.health.argonaut.api.samples.SampleDiagnosticReports;
+import lombok.SneakyThrows;
+
+import javax.xml.datatype.DatatypeFactory;
+
+public class DiagnosticReportTransformerTest {
+
+    private XmlSampleData cdw = new XmlSampleData();
+    private SampleDiagnosticReports diagnosticReport = SampleDiagnosticReports.get();
+
+    private static class XmlSampleData {
+
+        private DatatypeFactory datatypeFactory;
+
+        @SneakyThrows
+        private XmlSampleData() {
+            datatypeFactory = DatatypeFactory.newInstance();
+        }
+    }
+}
