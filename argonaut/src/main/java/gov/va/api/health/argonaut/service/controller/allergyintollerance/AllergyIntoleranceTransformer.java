a15e49c003268bd4f2e4cb6ebc0c1e5a46e20fe8
Emily Subocz <emily.subocz@libertyits.com>
2018-11-21 17:05:35 -0500

    allergy intolerace transformer work

--- argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/allergyintollerance/AllergyIntoleranceTransformer.java
+++ argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/allergyintollerance/AllergyIntoleranceTransformer.java
@@ -1,20 +1,22 @@
 package gov.va.api.health.argonaut.service.controller.allergyintollerance;
 
+import static gov.va.api.health.argonaut.service.controller.Transformers.asDateTimeString;
 import static gov.va.api.health.argonaut.service.controller.Transformers.convertAll;
 import static gov.va.api.health.argonaut.service.controller.Transformers.ifPresent;
 
-import gov.va.api.health.argonaut.api.datatypes.Annotation;
+import gov.va.api.health.argonaut.api.datatypes.CodeableConcept;
 import gov.va.api.health.argonaut.api.datatypes.Coding;
 import gov.va.api.health.argonaut.api.elements.Reference;
 import gov.va.api.health.argonaut.api.resources.AllergyIntolerance;
+import gov.va.api.health.argonaut.api.resources.AllergyIntolerance.Reaction;
 import gov.va.dvp.cdw.xsd.model.CdwAllergyIntolerance103Root.CdwAllergyIntolerances.CdwAllergyIntolerance;
-import gov.va.dvp.cdw.xsd.model.CdwAllergyIntolerance103Root.CdwAllergyIntolerances.CdwAllergyIntolerance.CdwNotes;
-import gov.va.dvp.cdw.xsd.model.CdwAllergyIntolerance103Root.CdwAllergyIntolerances.CdwAllergyIntolerance.CdwNotes.CdwNote;
-import gov.va.dvp.cdw.xsd.model.CdwAllergyIntolerance103Root.CdwAllergyIntolerances.CdwAllergyIntolerance.CdwSubstance;
-import gov.va.dvp.cdw.xsd.model.CdwAllergyIntoleranceStatus;
+import gov.va.dvp.cdw.xsd.model.CdwAllergyIntolerance103Root.CdwAllergyIntolerances.CdwAllergyIntolerance.CdwReactions;
+import gov.va.dvp.cdw.xsd.model.CdwAllergyIntolerance103Root.CdwAllergyIntolerances.CdwAllergyIntolerance.CdwReactions.CdwReaction.CdwManifestations.CdwManifestation;
+import gov.va.dvp.cdw.xsd.model.CdwAllergyManifestationSystem;
 import gov.va.dvp.cdw.xsd.model.CdwReference;
-import java.sql.Ref;
+import java.util.Collections;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,14 +30,16 @@ public class AllergyIntoleranceTransformer implements AllergyIntoleranceControll
     return AllergyIntolerance.builder()
         .id(source.getCdwId())
         .resourceType("AllergyIntolerance")
-        //.onset(source.getOnset())
-        //.recordedDate(source.getRecordedDate())
+        .onset(asDateTimeString(source.getOnset()))
+        .recordedDate(asDateTimeString(source.getRecordedDate()))
         .recorder(recorder(source.getRecorder()))
         .patient(patient(source.getPatient()))
         .status(ifPresent(source.getStatus(), status -> AllergyIntolerance.Status.valueOf(status.value())))
         .criticality(ifPresent(source.getCriticality(), criticality -> AllergyIntolerance.Criticality.valueOf(criticality.value())))
         .type(ifPresent(source.getType(), type -> AllergyIntolerance.Type.valueOf(type.value())))
         .category(ifPresent(source.getCategory(), category -> AllergyIntolerance.Category.valueOf(category.value())))
+        //come back to note
+        .reaction(reactions(source.getReactions()))
         .build();
   }
 
@@ -44,6 +48,36 @@ public class AllergyIntoleranceTransformer implements AllergyIntoleranceControll
         .display(source.getDisplay())
         .display(source.getReference())
         .build();
+  }
+
+  List<CodeableConcept> reactionManifestation(CdwManifestation source) {
+    if (source == null) {
+      return Collections.emptyList();
+    }
+    return Collections.singletonList(
+        CodeableConcept.builder()
+            .coding(reactionManifestationCoding(source.getCoding()))
+            .text(source.getText())
+            .build());
+  }
+
+  List<Coding> reactionManifestationCoding(CdwManifestation.CdwCoding source) {
+    return Collections.singletonList(
+        Coding.builder()
+            .system(ifPresent(source.getSystem(), CdwAllergyManifestationSystem::value))
+            .code(source.getCode())
+            .display(source.getDisplay())
+            .build());
+  }
+
+  List<Reaction> reactions(CdwReactions optionalSource) {
+    return convertAll(
+        ifPresent(optionalSource, CdwReactions::getReaction),
+        cdw ->
+            Reaction.builder()
+                .certainty(ifPresent(cdw.getCertainty(), certainty -> AllergyIntolerance.Certainty.valueOf(certainty.value())))
+                .manifestation(reactionManifestation(cdw.getManifestations().getManifestation().get(0))) // won't work, needs to be incremented... loop?
+                .build());
   }
 
   Reference recorder (CdwReference source) {
