b0fbffc24eef9c9e8b63ad5fc27b98aed722e9c6
Ian LaFlamme <ian.laflamme@libertyits.com>
2018-12-04 16:53:08 -0500

    appointment bundle

--- argonaut-api/src/main/java/gov/va/api/health/argonaut/api/resources/Appointment.java
+++ argonaut-api/src/main/java/gov/va/api/health/argonaut/api/resources/Appointment.java
@@ -3,18 +3,25 @@ package gov.va.api.health.argonaut.api.resources;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import gov.va.api.health.argonaut.api.Fhir;
+import gov.va.api.health.argonaut.api.bundle.AbstractBundle;
+import gov.va.api.health.argonaut.api.bundle.AbstractEntry;
+import gov.va.api.health.argonaut.api.bundle.BundleLink;
 import gov.va.api.health.argonaut.api.datatypes.CodeableConcept;
 import gov.va.api.health.argonaut.api.datatypes.Identifier;
+import gov.va.api.health.argonaut.api.datatypes.Signature;
 import gov.va.api.health.argonaut.api.datatypes.SimpleResource;
 import gov.va.api.health.argonaut.api.elements.BackboneElement;
 import gov.va.api.health.argonaut.api.elements.Extension;
 import gov.va.api.health.argonaut.api.elements.Meta;
 import gov.va.api.health.argonaut.api.elements.Narrative;
 import gov.va.api.health.argonaut.api.elements.Reference;
+import gov.va.api.health.argonaut.api.resources.Appointment.Entry;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -22,6 +29,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -81,6 +89,51 @@ public class Appointment implements Resource {
     fulfilled,
     cancelled,
     noshow
+  }
+
+  @Data
+  @NoArgsConstructor
+  @EqualsAndHashCode(callSuper = true)
+  @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+  @JsonDeserialize(builder = Appointment.Bundle.BundleBuilder.class)
+  @Schema(name = "AppointmentBundle")
+  public static class Bundle extends AbstractBundle<Entry> {
+    @Builder
+    public Bundle(
+        @NotBlank String resourceType,
+        @Pattern(regexp = Fhir.ID) String id,
+        @Valid Meta meta,
+        @Pattern(regexp = Fhir.URI) String implicitRules,
+        @Pattern(regexp = Fhir.CODE) String language,
+        @NotNull BundleType type,
+        @Min(0) Integer total,
+        @Valid List<BundleLink> link,
+        @Valid List<Entry> entry,
+        @Valid Signature signature) {
+      super(resourceType, id, meta, implicitRules, language, type, total, link, entry, signature);
+    }
+  }
+
+  @Data
+  @NoArgsConstructor
+  @EqualsAndHashCode(callSuper = true)
+  @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+  @JsonDeserialize(builder = Appointment.Entry.EntryBuilder.class)
+  @Schema(name = "AppointmentEntry")
+  public static class Entry extends AbstractEntry<Appointment> {
+    @Builder
+    public Entry(
+        @Pattern(regexp = Fhir.ID) String id,
+        @Valid List<Extension> extension,
+        @Valid List<Extension> modifierExtension,
+        @Valid List<BundleLink> link,
+        @Pattern(regexp = Fhir.URI) String fullUrl,
+        @Valid Appointment resource,
+        @Valid Search search,
+        @Valid Request request,
+        @Valid Response response) {
+      super(id, extension, modifierExtension, link, fullUrl, resource, search, request, response);
+    }
   }
 
   @Data
--- argonaut-api/src/test/java/gov/va/api/health/argonaut/api/resources/AppointmentTest.java
+++ argonaut-api/src/test/java/gov/va/api/health/argonaut/api/resources/AppointmentTest.java
@@ -2,7 +2,10 @@ package gov.va.api.health.argonaut.api.resources;
 
 import static gov.va.api.health.argonaut.api.RoundTrip.assertRoundTrip;
 
+import gov.va.api.health.argonaut.api.bundle.AbstractBundle;
+import gov.va.api.health.argonaut.api.bundle.BundleLink;
 import gov.va.api.health.argonaut.api.samples.SampleAppointments;
+import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -10,6 +13,40 @@ import org.junit.Test;
 public class AppointmentTest {
 
   private final SampleAppointments data = SampleAppointments.get();
+
+  @Test
+  public void bundlerCanBuildAppointmentBundles() {
+    Appointment.Entry entry =
+        Appointment.Entry.builder()
+            .extension(Collections.singletonList(data.extension()))
+            .fullUrl("http://Appointment.com")
+            .id("123")
+            .link(
+                Collections.singletonList(
+                    BundleLink.builder()
+                        .relation(BundleLink.LinkRelation.self)
+                        .url(("http://Appointment.com/1"))
+                        .build()))
+            .resource(data.appointment())
+            .search(data.search())
+            .request(data.request())
+            .response(data.response())
+            .build();
+
+    Appointment.Bundle bundle =
+        Appointment.Bundle.builder()
+            .entry(Collections.singletonList(entry))
+            .link(
+                Collections.singletonList(
+                    BundleLink.builder()
+                        .relation(BundleLink.LinkRelation.self)
+                        .url(("http://Appointment.com/2"))
+                        .build()))
+            .type(AbstractBundle.BundleType.searchset)
+            .build();
+
+    assertRoundTrip(bundle);
+  }
 
   @Test
   public void appointment() {
