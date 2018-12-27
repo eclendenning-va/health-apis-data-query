c847de66d728705c78ed539f0b51246624a986a8
Bryan Schofield <bryan.schofield@libertyits.com>
2018-12-27 16:58:32 -0500

    WIP - Crawler is crawling\! ... no validation support

--- sentinel/src/test/java/gov/va/health/api/sentinel/LabTest.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/LabTest.java
@@ -3,6 +3,10 @@ package gov.va.health.api.sentinel;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import gov.va.health.api.sentinel.categories.Lab;
+import gov.va.health.api.sentinel.crawler.ConcurrentRequestQueue;
+import gov.va.health.api.sentinel.crawler.Crawler;
+import gov.va.health.api.sentinel.crawler.RequestQueue;
+import gov.va.health.api.sentinel.crawler.ResourceDiscovery;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -13,5 +17,18 @@ public class LabTest {
   @Test
   public void login() {
     assertThat(robots.user1().token().accessToken()).isNotBlank();
+    ResourceDiscovery discovery =
+        ResourceDiscovery.builder()
+            .patientId(robots.userCredentials1().icn())
+            .url("https://dev-api.va.gov/services/argonaut/v0")
+            .build();
+    RequestQueue q = new ConcurrentRequestQueue();
+    discovery.queries().forEach(q::add);
+    Crawler crawler =
+        Crawler.builder()
+            .requestQueue(q)
+            .authenticationToken(() -> robots.user1().token().accessToken())
+            .build();
+    crawler.crawl();
   }
 }
