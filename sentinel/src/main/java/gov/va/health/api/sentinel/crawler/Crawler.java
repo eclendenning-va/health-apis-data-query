4edb8a3eac0ec3f0ef1e777ecf3e1950760c6d6f
Bryan Schofield <bryan.schofield@libertyits.com>
2018-12-28 13:51:19 -0500

    Added UrlToResourceConverter utility to Sentinel to support crawling. This will determine the resource type from a URL.

--- sentinel/src/main/java/gov/va/health/api/sentinel/crawler/Crawler.java
+++ sentinel/src/main/java/gov/va/health/api/sentinel/crawler/Crawler.java
@@ -3,9 +3,11 @@ package gov.va.health.api.sentinel.crawler;
 import io.restassured.RestAssured;
 import java.util.function.Supplier;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
 /** The Crawler will recursive request resources from an Argonaut server. I */
 @Builder
+@Slf4j
 public class Crawler {
 
   private final RequestQueue requestQueue;
@@ -14,11 +16,17 @@ public class Crawler {
   /** Crawler iterates through queue performing all queries. */
   public void crawl() {
     while (requestQueue.hasNext()) {
+      String url = requestQueue.next();
+
+      Class<?> type = new UrlToResourceConverter().apply(url);
+
+      log.info("Requesting {} as {}", url, type.getName());
+
       RestAssured.given()
           .header("Authorization", "Bearer " + authenticationToken.get())
           .contentType("application/fhir+json")
           // TODO .header("jargonaut", USE_JARGONAUT)
-          .get(requestQueue.next())
+          .get(url)
           .then()
           .log()
           .all();
