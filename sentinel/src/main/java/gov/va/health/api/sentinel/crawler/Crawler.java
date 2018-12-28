7c06f9c563f2437533ccd14f790badb60e456457
Bryan Schofield <bryan.schofield@libertyits.com>
2018-12-28 17:30:28 -0500

    WIP - Crawler reference links can now be followed. MedicationOrder now supports _prescriber field

--- sentinel/src/main/java/gov/va/health/api/sentinel/crawler/Crawler.java
+++ sentinel/src/main/java/gov/va/health/api/sentinel/crawler/Crawler.java
@@ -1,8 +1,10 @@
 package gov.va.health.api.sentinel.crawler;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import java.util.function.Supplier;
 import lombok.Builder;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 /** The Crawler will recursive request resources from an Argonaut server. I */
@@ -15,21 +17,42 @@ public class Crawler {
 
   /** Crawler iterates through queue performing all queries. */
   public void crawl() {
+    int count = 0; // TODO remove me
     while (requestQueue.hasNext()) {
+      count++;
       String url = requestQueue.next();
+      try {
+        process(url);
+      } catch (Exception e) {
+        log.error("Failed to process {}", url, e);
+        // TODO return result
+      }
+    }
+    log.info("Made {} requests", count); // TODO remove me
+  }
 
-      Class<?> type = new UrlToResourceConverter().apply(url);
-
-      log.info("Requesting {} as {}", url, type.getName());
-
-      RestAssured.given()
-          .header("Authorization", "Bearer " + authenticationToken.get())
-          .contentType("application/fhir+json")
-          // TODO .header("jargonaut", USE_JARGONAUT)
-          .get(url)
-          .then()
-          .log()
-          .all();
+  @SneakyThrows
+  private void process(String url) {
+    // TODO return result
+    Class<?> type = new UrlToResourceConverter().apply(url);
+    log.info("Requesting {} as {}", url, type.getName());
+    Response response =
+        RestAssured.given()
+            .header("Authorization", "Bearer " + authenticationToken.get())
+            .contentType("application/fhir+json")
+            // TODO .header("jargonaut", USE_JARGONAUT)
+            .get(url)
+            .andReturn();
+    if (response.getStatusCode() == 200) {
+      ReferenceInterceptor interceptor = new ReferenceInterceptor();
+      Object payload = interceptor.mapper().readValue(response.asByteArray(), type);
+      log.info(
+          "Found {} references in {}",
+          interceptor.references().size(),
+          payload.getClass().getName());
+      interceptor.references().forEach(u -> requestQueue.add(u));
+    } else {
+      // TODO return sad result
     }
   }
 }
