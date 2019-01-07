98b166718a5dc11c2d74b11f77b23c4180cf766a
alecbert-va <alec.bertossa.liberty@gmail.com>
2019-01-07 09:01:03 -0500

    Added tests and cleaned up model

--- argonaut-api/src/main/java/gov/va/api/health/argonaut/api/resources/MedicationDispense.java
+++ argonaut-api/src/main/java/gov/va/api/health/argonaut/api/resources/MedicationDispense.java
@@ -2,8 +2,18 @@ package gov.va.api.health.argonaut.api.resources;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import gov.va.api.health.argonaut.api.Fhir;
-import gov.va.api.health.argonaut.api.datatypes.*;
+import gov.va.api.health.argonaut.api.bundle.AbstractBundle;
+import gov.va.api.health.argonaut.api.bundle.AbstractEntry;
+import gov.va.api.health.argonaut.api.bundle.BundleLink;
+import gov.va.api.health.argonaut.api.datatypes.CodeableConcept;
+import gov.va.api.health.argonaut.api.datatypes.Identifier;
+import gov.va.api.health.argonaut.api.datatypes.SimpleQuantity;
+import gov.va.api.health.argonaut.api.datatypes.SimpleResource;
+import gov.va.api.health.argonaut.api.datatypes.Timing;
+import gov.va.api.health.argonaut.api.datatypes.Ratio;
+import gov.va.api.health.argonaut.api.datatypes.Signature;
 import gov.va.api.health.argonaut.api.elements.BackboneElement;
 import gov.va.api.health.argonaut.api.elements.Extension;
 import gov.va.api.health.argonaut.api.elements.Meta;
@@ -12,6 +22,7 @@ import gov.va.api.health.argonaut.api.elements.Reference;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -19,6 +30,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -125,5 +137,52 @@ public class MedicationDispense implements DomainResource {
         @Valid List<CodeableConcept> reason;
         @Valid List<Reference> responsibleParty;
     }
+
+    @Data
+    @NoArgsConstructor
+    @EqualsAndHashCode(callSuper = true)
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    @JsonDeserialize(builder = MedicationDispense.Entry.EntryBuilder.class)
+    @Schema(name = "MedicationDispenseEntry")
+    public static class Entry extends AbstractEntry<MedicationDispense> {
+
+        @Builder
+        public Entry(
+                @Pattern(regexp = Fhir.ID) String id,
+                @Valid List<Extension> extension,
+                @Valid List<Extension> modifierExtension,
+                @Valid List<BundleLink> link,
+                @Pattern(regexp = Fhir.URI) String fullUrl,
+                @Valid MedicationDispense resource,
+                @Valid Search search,
+                @Valid Request request,
+                @Valid Response response) {
+            super(id, extension, modifierExtension, link, fullUrl, resource, search, request, response);
+        }
+    }
+
+  @Data
+  @NoArgsConstructor
+  @EqualsAndHashCode(callSuper = true)
+  @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+  @JsonDeserialize(builder = MedicationDispense.Bundle.BundleBuilder.class)
+  @Schema(name = "MedicationDispenseBundle")
+  public static class Bundle extends AbstractBundle<Entry> {
+
+    @Builder
+    public Bundle(
+        @NotBlank String resourceType,
+        @Pattern(regexp = Fhir.ID) String id,
+        @Valid Meta meta,
+        @Pattern(regexp = Fhir.URI) String implicitRules,
+        @Pattern(regexp = Fhir.CODE) String language,
+        @NotNull BundleType type,
+        @Min(0) Integer total,
+        @Valid List<BundleLink> link,
+        @Valid List<MedicationDispense.Entry> entry,
+        @Valid Signature signature) {
+      super(resourceType, id, meta, implicitRules, language, type, total, link, entry, signature);
+    }
+   }
 
 }
--- argonaut-api/src/test/java/gov/va/api/health/argonaut/api/resources/MedicationDispenseTest.java
+++ argonaut-api/src/test/java/gov/va/api/health/argonaut/api/resources/MedicationDispenseTest.java
@@ -1,22 +1,58 @@
 package gov.va.api.health.argonaut.api.resources;
 
 import static gov.va.api.health.argonaut.api.RoundTrip.assertRoundTrip;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import gov.va.api.health.argonaut.api.ZeroOrOneOfVerifier;
 import gov.va.api.health.argonaut.api.bundle.AbstractBundle.BundleType;
+import gov.va.api.health.argonaut.api.bundle.AbstractEntry;
 import gov.va.api.health.argonaut.api.bundle.BundleLink;
 import gov.va.api.health.argonaut.api.bundle.BundleLink.LinkRelation;
-import gov.va.api.health.argonaut.api.resources.Observation.Bundle;
-import gov.va.api.health.argonaut.api.resources.Observation.Entry;
-import gov.va.api.health.argonaut.api.samples.SampleObservations;
+import gov.va.api.health.argonaut.api.samples.SampleMedicationDispenses;
+import gov.va.api.health.argonaut.api.resources.MedicationDispense.Bundle;
+import gov.va.api.health.argonaut.api.resources.MedicationDispense.Entry;
 import java.util.Collections;
-import java.util.Set;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.ValidatorFactory;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 public class MedicationDispenseTest {
+    private final SampleMedicationDispenses data = SampleMedicationDispenses.get();
+
+    @Test
+    public void bundlerCanBuildMedicationDispenseBundles() {
+        Entry entry =
+            Entry.builder()
+                .extension(Collections.singletonList(data.extension()))
+                .fullUrl("http://medicationdispense.com")
+                .id("123")
+                .link(
+                    Collections.singletonList(
+                        BundleLink.builder()
+                            .relation(LinkRelation.self)
+                            .url("http://medicationdispense/123")
+                            .build()))
+                .resource(data.medicationDispense())
+                .search(data.search())
+                .request(data.request())
+                .response(data.response())
+                .build();
+
+        Bundle bundle =
+            Bundle.builder()
+                .entry(Collections.singletonList(entry))
+                .link(
+                    Collections.singletonList(
+                        BundleLink.builder()
+                            .relation(LinkRelation.self)
+                            .url("https://medicationdispense?patient=456")
+                            .build()))
+                .type(BundleType.searchset)
+                .build();
+
+        assertRoundTrip(bundle);
+
+        //What is the point of this
+        AbstractEntry.Search.builder().build().id();
+    }
+
+    @Test
+    public void medicationDispense() {
+        assertRoundTrip(data.medicationDispense());
+    }
 }
