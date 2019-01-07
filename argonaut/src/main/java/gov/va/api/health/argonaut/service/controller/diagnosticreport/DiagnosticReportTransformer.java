fafd7947bd87a134c1b7aa6d3e10cc4e382c67e4
Emily Subocz <emily.subocz@libertyits.com>
2019-01-07 15:54:26 -0500

    Perfection is achieved...

--- argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/diagnosticreport/DiagnosticReportTransformer.java
+++ argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/diagnosticreport/DiagnosticReportTransformer.java
@@ -112,7 +112,7 @@ public class DiagnosticReportTransformer implements DiagnosticReportController.T
   }
 
   private boolean isUsable(CdwReference reference) {
-    return !(reference == null || allNull(reference.getDisplay(), reference.getReference()));
+    return reference != null && !allNull(reference.getDisplay(), reference.getReference());
   }
 
   Reference performer(CdwReference maybeReference) {
