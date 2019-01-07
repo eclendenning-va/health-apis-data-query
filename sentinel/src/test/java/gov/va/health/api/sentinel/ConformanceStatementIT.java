ba63c0f7ed82d96dce15c265dbcb16a595711cfe
Bryan Schofield <bryan.schofield@libertyits.com>
2019-01-07 14:42:49 -0500

    Simplified and combined conformance statement tab and newline test

--- sentinel/src/test/java/gov/va/health/api/sentinel/ConformanceStatementIT.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/ConformanceStatementIT.java
@@ -1,35 +1,18 @@
 package gov.va.health.api.sentinel;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import gov.va.api.health.argonaut.api.resources.Conformance;
 import org.junit.Test;
 
 public class ConformanceStatementIT {
   @Test
   public void conformanceStatementIsValid() {
-    Sentinel.get()
-        .clients()
-        .argonaut()
-        .get("/api/metadata")
-        .expect(200)
-        .expectValid(Conformance.class);
-  }
-
-  @Test
-  public void conformanceStatementShouldHaveNoNewlineOrTabChars() {
-    String conformanceBody =
-        Sentinel.get()
-            .clients()
-            .argonaut()
-            .service()
-            .requestSpecification()
-            .header("Accept", "application/json")
-            .get("/api/metadata")
-            .getBody()
-            .asString();
-
-    if (conformanceBody.contains("/n") || conformanceBody.contains("/t")) {
-      throw new AssertionError(
-          "Newline or Tab characters exist within the conformance statement JSON body");
-    }
+    ExpectedResponse response = Sentinel.get().clients().argonaut().get("/api/metadata");
+    response.expect(200).expectValid(Conformance.class);
+    String rawJson = response.response().asString();
+    assertThat(rawJson)
+        .withFailMessage("Tabs and newlines break our customer.")
+        .doesNotContain("\n", "\t");
   }
 }
