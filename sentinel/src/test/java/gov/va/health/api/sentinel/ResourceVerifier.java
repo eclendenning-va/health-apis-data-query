1b9f52687fdb63d1e6c0f48f799a8d1519c384e5
Bryan Schofield <bryan.schofield@libertyits.com>
2019-01-22 12:12:26 -0500

    Refactored read/search test into resource specific tests with supporting verifier test class

--- sentinel/src/test/java/gov/va/health/api/sentinel/ResourceVerifier.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/ResourceVerifier.java
@@ -2,33 +2,91 @@ package gov.va.health.api.sentinel;
 
 import gov.va.api.health.argonaut.api.bundle.AbstractBundle;
 import gov.va.api.health.argonaut.api.resources.OperationOutcome;
+import java.util.Arrays;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
-public class ResourceRequest {
+/** This support class can be used to test standard resource queries, such as reads and searches. */
+@Slf4j
+public class ResourceVerifier {
+  private static final ResourceVerifier INSTANCE = new ResourceVerifier();
+  @Getter private final TestClient argonaut = Sentinel.get().clients().argonaut();
+  @Getter private final TestIds ids = IdRegistrar.of(Sentinel.get().system()).registeredIds();
 
-  public static Object[] assertRequest(
-      int status, Class<?> response, String path, String... parameters) {
-    return new Object[] {status, response, path, parameters};
+  public static ResourceVerifier get() {
+    return INSTANCE;
   }
 
-  private TestClient argonaut() {
-    return Sentinel.get().clients().argonaut();
-  }
-
-  public void getResource(String path, String[] params, int status, Class<?> response) {
-    argonaut().get(path, params).expect(status).expectValid(response);
+  public static <T> TestCase<T> test(
+      int status, Class<T> response, String path, String... parameters) {
+    return TestCase.<T>builder()
+        .path(path)
+        .parameters(parameters)
+        .response(response)
+        .status(status)
+        .build();
   }
 
   /**
    * If the response is a bundle, then the query is a search. We want to verify paging parameters
    * restrict page >= 1, _count >=1, and _count <= 20
    */
-  public void pagingParameterBounds(String path, String[] params, Class<?> response) {
-    if (!AbstractBundle.class.isAssignableFrom(response)) {
+  public <T> void assertPagingParameterBounds(TestCase<T> tc) {
+    if (!AbstractBundle.class.isAssignableFrom(tc.response())) {
       return;
     }
-    argonaut().get(path + "&page=0", params).expect(400).expectValid(OperationOutcome.class);
-    argonaut().get(path + "&_count=-1", params).expect(400).expectValid(OperationOutcome.class);
-    argonaut().get(path + "&_count=0", params).expect(200).expectValid(response);
-    argonaut().get(path + "&_count=21", params).expect(200).expectValid(response);
+    argonaut()
+        .get(tc.path() + "&page=0", tc.parameters())
+        .expect(400)
+        .expectValid(OperationOutcome.class);
+    argonaut()
+        .get(tc.path() + "&_count=-1", tc.parameters())
+        .expect(400)
+        .expectValid(OperationOutcome.class);
+    argonaut().get(tc.path() + "&_count=0", tc.parameters()).expect(200).expectValid(tc.response());
+    argonaut()
+        .get(tc.path() + "&_count=21", tc.parameters())
+        .expect(200)
+        .expectValid(tc.response());
+  }
+
+  public <T> T assertRequest(TestCase<T> tc) {
+    return argonaut()
+        .get(tc.path(), tc.parameters())
+        .expect(tc.status())
+        .expectValid(tc.response());
+  }
+
+  public <T> T verify(TestCase<T> tc) {
+    assertPagingParameterBounds(tc);
+    return assertRequest(tc);
+  }
+
+  public void verifyAll(TestCase<?>... testCases) {
+    log.info("Verifying {} test cases", testCases.length);
+    for (TestCase tc : testCases) {
+      try {
+        verify(tc);
+      } catch (Exception e) {
+        log.error(
+            "Failure: {} with parameters {}: {}",
+            tc.path(),
+            Arrays.toString(tc.parameters()),
+            e.getMessage());
+        throw e;
+      }
+    }
+    log.info("Verified {} test cases", testCases.length);
+  }
+
+  @Value
+  @Builder
+  public static class TestCase<T> {
+    int status;
+    Class<T> response;
+    String path;
+    String[] parameters;
   }
 }
