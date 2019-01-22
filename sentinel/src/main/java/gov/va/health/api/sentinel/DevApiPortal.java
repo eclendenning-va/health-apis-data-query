ccffc9ab1c2dbce411fba59a0a1f9e3c87d3a29c
Evan Clendenning <evan.clendenning@libertyits.com>
2019-01-22 15:14:43 -0500

    check correct urls for documentation now

--- sentinel/src/main/java/gov/va/health/api/sentinel/DevApiPortal.java
+++ sentinel/src/main/java/gov/va/health/api/sentinel/DevApiPortal.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
@@ -12,16 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 @Slf4j
-public class GeneratedSwaggerConsole {
+public class DevApiPortal {
 
   private ChromeDriver driver;
   private WebDriverWait wait;
@@ -59,9 +55,10 @@ public class GeneratedSwaggerConsole {
   }
 
   /** Set up the chrome driver with the proper properties. */
-  public void initializeDriver(int defaultTimeOutInSeconds) {
+  public void initializeDriver(String url, int defaultTimeOutInSeconds) {
     Config config = new Config(new File("config/lab.properties"));
     ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--whitelisted-ips", "--disable-extensions", "--no-sandbox");
     chromeOptions.setHeadless(config.headless());
     if (StringUtils.isNotBlank(config.driver())) {
       System.setProperty("webdriver.chrome.driver", config.driver());
@@ -69,59 +66,24 @@ public class GeneratedSwaggerConsole {
     driver = new ChromeDriver(chromeOptions);
     wait = new WebDriverWait(driver, defaultTimeOutInSeconds);
     driver.manage().timeouts().implicitlyWait(defaultTimeOutInSeconds, TimeUnit.SECONDS);
-    driver.get("https://argonaut.lighthouse.va.gov/console/");
+    driver.get(url);
   }
 
-  public String title() {
-    return driver.getTitle();
+  public WebElement getElement(By by) {
+    return driver.findElement(by);
   }
 
-  /** Return a list of resources listed on the console. */
-  public List<WebElement> resources() {
-    return driver
-        .findElement(By.id("raml-console-resources-container"))
-        .findElements(By.tagName("li"));
+  public WebElement getChild(WebElement element, By by) {
+    return element.findElement(by);
   }
 
-  /** Return a list of content types available for a resource. */
-  public List<WebElement> contentTypes() {
-    return driver
-        .findElement(By.className("raml-console-resource-body-heading"))
-        .findElements(By.tagName("span"));
-  }
-
-  /** Clicks the child found by class name on the element provided. */
-  public void clickChild(WebElement element, String childClassName) {
-    wait.until(ExpectedConditions.elementToBeClickable(By.className(childClassName)));
-    try {
-      click(element.findElement(By.className(childClassName)));
-    } catch (NoSuchElementException e) {
-      log.error("Child does not exist");
-    }
-  }
-
-  /** Clicks the element provided. */
-  public void click(WebElement element) {
-    JavascriptExecutor js = (JavascriptExecutor) driver;
-    wait.until(ExpectedConditions.elementToBeClickable(element));
-    js.executeScript("arguments[0].click()", element);
-  }
-
-  /** Tears down driver. */
   public void quit() {
     driver.quit();
   }
 
-  /** Returns true if the element exists, and false if not. */
+  /** Returns true if the element exists, and errors if not. */
   public boolean isElementPresent(By by) {
-    wait.until(ExpectedConditions.presenceOfElementLocated(by));
-    try {
-      driver.findElement(by);
-      log.info("Exists");
-      return true;
-    } catch (NoSuchElementException e) {
-      log.info("Does Not Exist");
-      return false;
-    }
+    driver.findElement(by);
+    return true;
   }
 }
--- sentinel/src/test/java/gov/va/health/api/sentinel/SwaggerAvailabilityTest.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/SwaggerAvailabilityTest.java
@@ -2,8 +2,8 @@ package gov.va.health.api.sentinel;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.google.common.collect.Iterables;
 import gov.va.health.api.sentinel.categories.Lab;
+import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -14,20 +14,30 @@ import org.openqa.selenium.WebElement;
 @Slf4j
 public class SwaggerAvailabilityTest {
 
+  DevApiPortal apiPortalPage = new DevApiPortal();
+
+  public void checkAvailability(String url) {
+    log.info("Checking {} for API Documentation", url);
+    apiPortalPage.initializeDriver(url, 10);
+    assertThat(apiPortalPage.isElementPresent(By.className("swagger-ui"))).isTrue();
+    WebElement headerGroup = apiPortalPage.getElement(By.tagName("hgroup"));
+    assertThat(headerGroup).isNotNull();
+    WebElement link = apiPortalPage.getChild(headerGroup, By.partialLinkText("openapi.json"));
+    assertThat(link).isNotNull();
+    RestAssured.baseURI = link.getText();
+    log.info("Testing that the discovered link {} returns 200", RestAssured.baseURI);
+    assertThat(RestAssured.given().get().getStatusCode()).isEqualTo(200);
+    log.info("API Documentation is available at {}", url);
+    apiPortalPage.quit();
+  }
+
   @Test
-  public void checkAvailability() {
-    GeneratedSwaggerConsole swaggerPage = new GeneratedSwaggerConsole();
-    swaggerPage.initializeDriver(10);
-    log.info(swaggerPage.title());
-    for (WebElement w : Iterables.skip(swaggerPage.resources(), 1)) {
-      log.info("---{}---", w.getAttribute("id"));
-      swaggerPage.clickChild(w, "raml-console-tab-get");
-      for (WebElement e : swaggerPage.contentTypes()) {
-        log.info(e.getText());
-        swaggerPage.click(e);
-        assertThat(swaggerPage.isElementPresent(By.className("json"))).isTrue();
-      }
-    }
-    swaggerPage.quit();
+  public void checkDevDevAvailability() {
+    checkAvailability("https://dev-developer.va.gov/explore/health/docs/argonaut");
+  }
+
+  @Test
+  public void checkDevAvailability() {
+    checkAvailability("https://developer.va.gov/explore/health/docs/argonaut");
   }
 }
