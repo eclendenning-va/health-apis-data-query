c6b39f2550ad268c67bb1b21534f9c46ceaa2e6a
Emily Subocz <emily.subocz@libertyits.com>
2019-01-22 09:29:08 -0500

    Refactor repeated data into methods, edit env URLs

--- sentinel/src/main/java/gov/va/health/api/sentinel/SystemDefinitions.java
+++ sentinel/src/main/java/gov/va/health/api/sentinel/SystemDefinitions.java
@@ -13,6 +13,31 @@ import lombok.Value;
 @NoArgsConstructor(staticName = "get")
 public class SystemDefinitions {
 
+  DiagnosticReports diagnosticReports() {
+    return DiagnosticReports.builder()
+        .loinc1("10000-8")
+        .loinc2("99999-9")
+        .onDate("eq1970-01-01")
+        .fromDate("gt1970-01-01")
+        .toDate("lt2038-01-01")
+        .dateYear("ge1970")
+        .dateYearMonth("ge1970-01")
+        .dateYearMonthDay("ge1970-01-01")
+        .dateYearMonthDayHour("ge1970-01-01T07")
+        .dateYearMonthDayHourMinute("ge1970-01-01T07:00")
+        .dateYearMonthDayHourMinuteSecond("ge1970-01-01T07:00:00")
+        .dateYearMonthDayHourMinuteSecondTimezone("ge1970-01-01T07:00:00+05:00")
+        .dateYearMonthDayHourMinuteSecondZulu("ge1970-01-01T07:00:00Z")
+        .dateGreaterThan("ge1970-01-01")
+        .dateNotEqual("ne1970-01-01")
+        .dateStartsWith("sa1970-01-01")
+        .dateNoPrefix("1970-01-01")
+        .dateEqual("1970-01-01")
+        .dateLessOrEqual("le2038-01-19")
+        .dateLessThan("lt2038-01-19")
+        .build();
+  }
+
   SystemDefinition local =
       SystemDefinition.builder()
           .ids(ServiceDefinition.builder().url("https://localhost").port(8089).build())
@@ -25,42 +50,14 @@ public class SystemDefinitions {
                   .condition("1400007575530:P")
                   .diagnosticReport("1000000031384:L")
                   .encounter("1200753214085")
-                  .diagnosticReports(
-                      DiagnosticReports.builder()
-                          .loinc1("10000-8")
-                          .loinc2("99999-9")
-                          .onDate("eq1970-01-01")
-                          .fromDate("gt1970-01-01")
-                          .toDate("lt2038-01-01")
-                          .dateYear("ge1970")
-                          .dateYearMonth("ge1970-01")
-                          .dateYearMonthDay("ge1970-01-01")
-                          .dateYearMonthDayHour("ge1970-01-01T07")
-                          .dateYearMonthDayHourMinute("ge1970-01-01T07:00")
-                          .dateYearMonthDayHourMinuteSecond("ge1970-01-01T07:00:00")
-                          .dateYearMonthDayHourMinuteSecondTimezone("ge1970-01-01T07:00:00+05:00")
-                          .dateYearMonthDayHourMinuteSecondZulu("ge1970-01-01T07:00:00Z")
-                          .dateGreaterThan("ge1970-01-01")
-                          .dateNotEqual("ne1970-01-01")
-                          .dateStartsWith("sa1970-01-01")
-                          .dateNoPrefix("1970-01-01")
-                          .dateEqual("1970-01-01")
-                          .dateLessOrEqual("le2038-01-19")
-                          .dateLessThan("lt2038-01-19")
-                          .build())
+                  .diagnosticReports(diagnosticReports())
                   .immunization("1000000043979")
                   .location("166365:L")
                   .medication("212846")
                   .medicationOrder("1200389904206:O")
                   .medicationStatement("1400000182116")
                   .observation("1201051417263:V")
-                  .observations(
-                      Observations.builder()
-                          .loinc1("72166-2")
-                          .loinc2("777-3")
-                          .onDate("2015-04-15")
-                          .dateRange(Range.allTime())
-                          .build())
+                  .observations(observations())
                   .organization("1000025431:C")
                   .pii(
                       PersonallyIdentifiableInformation.builder()
@@ -73,51 +70,39 @@ public class SystemDefinitions {
                   .patient("185601V825290")
                   .practitioner("10092125")
                   .procedure("1400000140034")
-                  .procedures(
-                      Procedures.builder()
-                          .onDate("ge2009")
-                          .fromDate("ge2009")
-                          .toDate("le2010")
-                          .build())
+                  .procedures(procedures())
                   .unknown("5555555555555")
                   .build())
           .build();
+
+  private Observations observations() {
+    return Observations.builder()
+        .loinc1("72166-2")
+        .loinc2("777-3")
+        .onDate("2015-04-15")
+        .dateRange(Range.allTime())
+        .build();
+  }
+
+  Procedures procedures() {
+    return Procedures.builder().fromDate("ge2009").onDate("ge2009").toDate("le2010").build();
+  }
 
   SystemDefinition prod =
       SystemDefinition.builder()
           .ids(
               ServiceDefinition.builder()
-                  .url("https://argonaut.lighthouse.va.gov/api")
+                  .url("https://argonaut.lighthouse.va.gov")
                   .port(443)
                   .build())
           .mrAnderson(
               ServiceDefinition.builder()
-                  .url("https://argonaut.lighthouse.va.gov/api")
+                  .url("https://argonaut.lighthouse.va.gov")
                   .port(443)
                   .build())
           .argonaut(
               ServiceDefinition.builder()
-                  .url("https://argonaut.lighthouse.va.gov/api")
-                  .port(443)
-                  .build())
-          .cdwIds(prodAndQaIds())
-          .build();
-
-  SystemDefinition qa =
-      SystemDefinition.builder()
-          .ids(
-              ServiceDefinition.builder()
-                  .url("https://qa-argonaut.lighthouse.va.gov/api")
-                  .port(443)
-                  .build())
-          .mrAnderson(
-              ServiceDefinition.builder()
-                  .url("https://qa-argonaut.lighthouse.va.gov/api")
-                  .port(443)
-                  .build())
-          .argonaut(
-              ServiceDefinition.builder()
-                  .url("https://qa-argonaut.lighthouse.va.gov/api")
+                  .url("https://argonaut.lighthouse.va.gov")
                   .port(443)
                   .build())
           .cdwIds(prodAndQaIds())
@@ -129,29 +114,7 @@ public class SystemDefinitions {
         .appointment("800295609816")
         .condition("800000007353:P")
         .diagnosticReport("800000005788:M")
-        .diagnosticReports(
-            DiagnosticReports.builder()
-                .loinc1("10000-8")
-                .loinc2("99999-9")
-                .onDate("eq1970-01-01")
-                .fromDate("gt1970-01-01")
-                .toDate("lt2038-01-01")
-                .dateYear("ge1970")
-                .dateYearMonth("ge1970-01")
-                .dateYearMonthDay("ge1970-01-01")
-                .dateYearMonthDayHour("ge1970-01-01T07")
-                .dateYearMonthDayHourMinute("ge1970-01-01T07:00")
-                .dateYearMonthDayHourMinuteSecond("ge1970-01-01T07:00:00")
-                .dateYearMonthDayHourMinuteSecondTimezone("ge1970-01-01T07:00:00+05:00")
-                .dateYearMonthDayHourMinuteSecondZulu("ge1970-01-01T07:00:00Z")
-                .dateGreaterThan("gt1970-01-01")
-                .dateNotEqual("ne1970-01-01")
-                .dateStartsWith("sa1970-01-01")
-                .dateNoPrefix("1970-01-01")
-                .dateEqual("eq1970-01-01")
-                .dateLessOrEqual("le2038-01-19")
-                .dateLessThan("lt2038-01-19")
-                .build())
+        .diagnosticReports(diagnosticReports())
         .encounter("1200444375877")
         .immunization("809653231")
         .location("279467:L")
@@ -159,20 +122,33 @@ public class SystemDefinitions {
         .medicationOrder("800130510958:O")
         .medicationStatement("800000000706")
         .observation("800436526883:V")
-        .observations(
-            Observations.builder()
-                .loinc1("72166-2")
-                .loinc2("777-3")
-                .onDate("2015-04-15")
-                .dateRange(Range.allTime())
-                .build())
+        .observations(observations())
         .organization("26366")
         .patient("1011537977V693883")
         .practitioner("1294240")
         .procedure("1200002649508")
-        .procedures(
-            Procedures.builder().fromDate("ge2009").onDate("ge2009").toDate("le2010").build())
+        .procedures(procedures())
         .unknown("5555555555555")
         .build();
   }
+
+  SystemDefinition qa =
+      SystemDefinition.builder()
+          .ids(
+              ServiceDefinition.builder()
+                  .url("https://qa-argonaut.lighthouse.va.gov")
+                  .port(443)
+                  .build())
+          .mrAnderson(
+              ServiceDefinition.builder()
+                  .url("https://qa-argonaut.lighthouse.va.gov")
+                  .port(443)
+                  .build())
+          .argonaut(
+              ServiceDefinition.builder()
+                  .url("https://qa-argonaut.lighthouse.va.gov")
+                  .port(443)
+                  .build())
+          .cdwIds(prodAndQaIds())
+          .build();
 }
