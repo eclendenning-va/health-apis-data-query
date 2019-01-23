2347aea9e8a8aae9898184adb458e2d75d1a8e7a
Ian LaFlamme <ian.laflamme@libertyits.com>
2019-01-23 14:22:37 -0500

    made jargonaut header a system property

--- sentinel/src/main/java/gov/va/health/api/sentinel/ServiceDefinition.java
+++ sentinel/src/main/java/gov/va/health/api/sentinel/ServiceDefinition.java
@@ -7,8 +7,11 @@ import java.util.function.Supplier;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 /** Defines particulars for interacting with a specific service. */
+@Slf4j
 @Value
 @Builder
 @AllArgsConstructor
@@ -24,9 +27,10 @@ public class ServiceDefinition {
             .port(port())
             .relaxedHTTPSValidation()
             .log()
-            .ifValidationFails()
-            .header("jargonaut", "true");
-
+            .ifValidationFails();
+    if (!StringUtils.isBlank(System.getenv("jargonaut"))) {
+      spec.header("jargonaut", System.getenv("jargonaut"));
+    }
     Optional<String> token = accessToken.get();
     if (token.isPresent()) {
       spec = spec.header("Authorization", "Bearer " + token.get());
