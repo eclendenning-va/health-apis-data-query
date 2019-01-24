a7430ef6eb0d71730ad4c05ecfed13d5dce9aced
Ian LaFlamme <ian.laflamme@libertyits.com>
2019-01-24 14:24:35 -0500

    Added env parsing to sentinel. Refactored patient read test to be smrter

--- sentinel/src/test/java/gov/va/health/api/sentinel/PatientIT.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/PatientIT.java
@@ -4,6 +4,7 @@ import static gov.va.health.api.sentinel.ResourceVerifier.test;
 
 import gov.va.api.health.argonaut.api.resources.OperationOutcome;
 import gov.va.api.health.argonaut.api.resources.Patient;
+import gov.va.health.api.sentinel.Sentinel.Environment;
 import gov.va.health.api.sentinel.categories.Prod;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -46,9 +47,24 @@ public class PatientIT {
   public void basic() {
     verifier.verifyAll(
         test(200, Patient.class, "/api/Patient/{id}", verifier.ids().patient()),
-        test(404, OperationOutcome.class, "/api/Patient/{id}", verifier.ids().unknown()),
         test(200, Patient.Bundle.class, "/api/Patient?_id={id}", verifier.ids().patient()),
         test(200, Patient.Bundle.class, "/api/Patient?identifier={id}", verifier.ids().patient()),
         test(404, OperationOutcome.class, "/api/Patient?_id={id}", verifier.ids().unknown()));
+  }
+
+  /**
+   * In the PROD/QA environments, patient reading is restricted to your unique access-token. Any IDs
+   * but your own are revoked with a 403 Forbidden. In environments where this restriction is
+   * lifted, the result of an unknown ID should be 404 Not Found.
+   */
+  @Test
+  public void patientMatching() {
+    if (Sentinel.environment() == Environment.LOCAL) {
+      verifier.verify(
+          test(404, OperationOutcome.class, "/api/Patient/{id}", verifier.ids().unknown()));
+    } else {
+      verifier.verify(
+          test(403, OperationOutcome.class, "/api/Patient/{id}", verifier.ids().unknown()));
+    }
   }
 }
