cf9565e1ac75987e1de6674a5705f92e4aa53779
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-01-31 16:39:02 -0500

    Remove unused logger.

--- argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/conformance/MetadataController.java
+++ argonaut/src/main/java/gov/va/api/health/argonaut/service/controller/conformance/MetadataController.java
@@ -32,14 +32,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Singular;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping(
   value = {"/api/metadata"},
