69a129ff39b8c48777c68e9a5d8961b06bc0e80d
bryan.schofield <vasdvp.jenkins@libertyits.com>
2019-05-01 15:55:00 +0000

    Release 1.0.204 - Jenkins Build 13 (department-of-veterans-affairs/health-apis-data-query/master)

--- data-query-api/src/test/java/gov/va/api/health/dataquery/api/datatypes/SignatureTest.java
+++ data-query-api/src/test/java/gov/va/api/health/dataquery/api/datatypes/SignatureTest.java
@@ -3,9 +3,7 @@ package gov.va.api.health.dataquery.api.datatypes;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import gov.va.api.health.dataquery.api.samples.SampleDataTypes;
-
 import java.util.Set;
-
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import org.junit.Test;
@@ -13,8 +11,41 @@ import org.junit.Test;
 public class SignatureTest {
 
   @Test
+  public void signatureMissingBothWhoUriAndWhoReferenceIsInvalid() {
+    Signature psuedoSignature =
+        Signature.builder()
+            .id("1234")
+            .type(SampleDataTypes.get().codingList())
+            .when("2000-01-01T00:00:00-00:00")
+            .contentType("contentTypeTest")
+            .blob("aGVsbG8=")
+            .build();
+    Set<ConstraintViolation<Signature>> problems =
+        Validation.buildDefaultValidatorFactory().getValidator().validate(psuedoSignature);
+    assertThat(problems.size()).isEqualTo(1);
+  }
+
+  @Test
+  public void signatureWithBothWhoUriAndWhoReferenceIsInvalid() {
+    Signature psuedoSignature =
+        Signature.builder()
+            .id("1234")
+            .type(SampleDataTypes.get().codingList())
+            .when("2000-01-01T00:00:00-00:00")
+            .whoUri("123456")
+            .whoReference(SampleDataTypes.get().reference())
+            .contentType("contentTypeTest")
+            .blob("aGVsbG8=")
+            .build();
+    Set<ConstraintViolation<Signature>> problems =
+        Validation.buildDefaultValidatorFactory().getValidator().validate(psuedoSignature);
+    assertThat(problems.size()).isEqualTo(1);
+  }
+
+  @Test
   public void signatureWithOnlyWhoReferenceIsValid() {
-    Signature psuedoSignature = Signature.builder()
+    Signature psuedoSignature =
+        Signature.builder()
             .id("1234")
             .type(SampleDataTypes.get().codingList())
             .when("2000-01-01T00:00:00-00:00")
@@ -22,18 +53,15 @@ public class SignatureTest {
             .contentType("contentTypeTest")
             .blob("aGVsbG8=")
             .build();
-
-    Set<ConstraintViolation<Signature>> problems = Validation
-        .buildDefaultValidatorFactory()
-        .getValidator()
-        .validate(psuedoSignature);
-
+    Set<ConstraintViolation<Signature>> problems =
+        Validation.buildDefaultValidatorFactory().getValidator().validate(psuedoSignature);
     assertThat(problems.size()).isEqualTo(0);
   }
 
   @Test
   public void signatureWithOnlyWhoUriIsValid() {
-    Signature psuedoSignature = Signature.builder()
+    Signature psuedoSignature =
+        Signature.builder()
             .id("1234")
             .type(SampleDataTypes.get().codingList())
             .when("2000-01-01T00:00:00-00:00")
@@ -41,50 +69,8 @@ public class SignatureTest {
             .contentType("contentTypeTest")
             .blob("aGVsbG8=")
             .build();
-
-    Set<ConstraintViolation<Signature>> problems = Validation
-        .buildDefaultValidatorFactory()
-        .getValidator()
-        .validate(psuedoSignature);
-
+    Set<ConstraintViolation<Signature>> problems =
+        Validation.buildDefaultValidatorFactory().getValidator().validate(psuedoSignature);
     assertThat(problems.size()).isEqualTo(0);
-  }
-
-  @Test
-  public void signatureWithBothWhoUriAndWhoReferenceIsInvalid() {
-    Signature psuedoSignature = Signature.builder()
-            .id("1234")
-            .type(SampleDataTypes.get().codingList())
-            .when("2000-01-01T00:00:00-00:00")
-            .whoUri("123456")
-            .whoReference(SampleDataTypes.get().reference())
-            .contentType("contentTypeTest")
-            .blob("aGVsbG8=")
-            .build();
-
-    Set<ConstraintViolation<Signature>> problems = Validation
-        .buildDefaultValidatorFactory()
-        .getValidator()
-        .validate(psuedoSignature);
-
-    assertThat(problems.size()).isEqualTo(1);
-  }
-
-  @Test
-  public void signatureMissingBothWhoUriAndWhoReferenceIsInvalid() {
-    Signature psuedoSignature = Signature.builder()
-            .id("1234")
-            .type(SampleDataTypes.get().codingList())
-            .when("2000-01-01T00:00:00-00:00")
-            .contentType("contentTypeTest")
-            .blob("aGVsbG8=")
-            .build();
-
-    Set<ConstraintViolation<Signature>> problems = Validation
-        .buildDefaultValidatorFactory()
-        .getValidator()
-        .validate(psuedoSignature);
-
-    assertThat(problems.size()).isEqualTo(1);
   }
 }
