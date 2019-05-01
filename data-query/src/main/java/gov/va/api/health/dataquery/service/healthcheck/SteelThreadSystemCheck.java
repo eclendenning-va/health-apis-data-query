ac8ead9fa84a1b1fb2799f81d626635494ca5d1e
bryan.schofield <vasdvp.jenkins@libertyits.com>
2019-05-01 15:48:50 +0000

    Release 1.0.203 - Jenkins Build 12 (department-of-veterans-affairs/health-apis-data-query/master)

--- data-query/src/main/java/gov/va/api/health/dataquery/service/healthcheck/SteelThreadSystemCheck.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/healthcheck/SteelThreadSystemCheck.java
@@ -83,8 +83,8 @@ public class SteelThreadSystemCheck implements HealthIndicator {
    * Frequency is configurable via properties.
    */
   @Scheduled(
-      fixedDelayString = "${health-check.read-frequency-ms}",
-      initialDelayString = "${health-check.read-frequency-ms}"
+    fixedDelayString = "${health-check.read-frequency-ms}",
+    initialDelayString = "${health-check.read-frequency-ms}"
   )
   @SneakyThrows
   public void runSteelThreadCheckAsynchronously() {
