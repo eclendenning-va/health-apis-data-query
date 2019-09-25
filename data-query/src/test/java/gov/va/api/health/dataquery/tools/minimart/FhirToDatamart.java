133387ec231c0f31494e991e7d0819e99a6b80c3
Evan Clendenning <evan.clendenning@libertyits.com>
2019-09-25 16:47:55 -0400

    use report.identifier to differentiate diagnostic report minimart files (#330)
    * use identifier for each diagnostic report to differentiate minimart files
    
    * remove colons from output file names

--- data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/FhirToDatamart.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/FhirToDatamart.java
@@ -68,8 +68,9 @@ public class FhirToDatamart {
     if (!outputDirectory.exists()) {
       outputDirectory.mkdir();
     }
-    log.info("Outputting to " + outputDirectory + "/dm" + fileName);
-    mapper.writeValue(new File(outputDirectory + "/dm" + fileName), object);
+    String outputPath = outputDirectory + "/dm" + fileName.replaceAll(":", "");
+    log.info("Outputting to " + outputPath);
+    mapper.writeValue(new File(outputPath), object);
   }
 
   @SneakyThrows
@@ -150,7 +151,7 @@ public class FhirToDatamart {
             .stream()
             .map(AbstractEntry::resource)
             .map(diagnosticReportTransformer::fhirToDatamart)
-            .forEach(d -> dmObjectToFile("DiaRep" + d.fullIcn() + ".json", d));
+            .forEach(d -> dmObjectToFile("DiaRep" + d.reports().get(0).identifier() + ".json", d));
         break;
       case "Immunization":
         F2DImmunizationTransformer immunizationTransformer =
