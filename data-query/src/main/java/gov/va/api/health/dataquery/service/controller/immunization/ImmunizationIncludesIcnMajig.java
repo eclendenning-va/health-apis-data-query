ec9a285df36128776c82117eb4c0c1089dd06d3c
Evan Clendenning <evan.clendenning@libertyits.com>
2019-10-15 17:29:22 -0400

    Immunization Patient Matching (#347)
    * `X-VA-INCLUDE-ICN` header available for kong patient matching with Immunization resource

---
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/immunization/ImmunizationIncludesIcnMajig.java
@@ -0,0 +1,25 @@
+package gov.va.api.health.dataquery.service.controller.immunization;
+
+import gov.va.api.health.argonaut.api.resources.Immunization;
+import gov.va.api.health.dataquery.service.controller.AbstractIncludesIcnMajig;
+import gov.va.api.health.dataquery.service.controller.Transformers;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * Intercept all RequestMapping payloads of Type Immunization.class or Bundle.class. Extract ICN(s)
+ * from these payloads with the provided function. This will lead to populating the
+ * X-VA-INCLUDES-ICN header.
+ */
+@ControllerAdvice
+public class ImmunizationIncludesIcnMajig extends AbstractIncludesIcnMajig<Immunization, Immunization.Entry, Immunization.Bundle> {
+    /** Converts the reference to a Datamart Reference to pull out the patient id. */
+    public ImmunizationIncludesIcnMajig() {
+        super(
+                Immunization.class,
+                Immunization.Bundle.class,
+                body -> Stream.ofNullable(Transformers.asReferenceId(body.patient())));
+    }
+}
---
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/immunization/ImmunizationIncludesIcnMajigTest.java
@@ -0,0 +1,24 @@
+package gov.va.api.health.dataquery.service.controller.immunization;
+
+import gov.va.api.health.argonaut.api.resources.Immunization;
+import gov.va.api.health.dataquery.service.controller.ExtractIcnValidator;
+import gov.va.api.health.dstu2.api.elements.Reference;
+import java.util.List;
+import org.junit.Test;
+
+public class ImmunizationIncludesIcnMajigTest {
+
+    @Test
+    public void extractIcn() {
+        ExtractIcnValidator.<ImmunizationIncludesIcnMajig, Immunization>builder()
+                .majig(new ImmunizationIncludesIcnMajig())
+                .body(
+                        Immunization.builder()
+                                .id("123")
+                                .patient(Reference.builder().reference("Patient/1010101010V666666").build())
+                                .build())
+                .expectedIcns(List.of("1010101010V666666"))
+                .build()
+                .assertIcn();
+    }
+}
