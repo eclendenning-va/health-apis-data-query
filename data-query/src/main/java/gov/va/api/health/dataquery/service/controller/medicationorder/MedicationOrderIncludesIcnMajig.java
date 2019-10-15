8578c33d279d6096476d417a4fa60bc3c37de546
bryan.schofield <vasdvp.jenkins@libertyits.com>
2019-10-15 21:43:44 +0000

    Release 2.0.121 - Jenkins Build 346 (department-of-veterans-affairs/health-apis-data-query/master)

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medicationorder/MedicationOrderIncludesIcnMajig.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medicationorder/MedicationOrderIncludesIcnMajig.java
@@ -7,18 +7,19 @@ import java.util.stream.Stream;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 /**
- * Intercept all RequestMapping payloads of Type MedicationOrder.class or Bundle.class. Extract ICN(s)
- * from these payloads with the provided function. This will lead to populating the
+ * Intercept all RequestMapping payloads of Type MedicationOrder.class or Bundle.class. Extract
+ * ICN(s) from these payloads with the provided function. This will lead to populating the
  * X-VA-INCLUDES-ICN header.
  */
 @ControllerAdvice
 public class MedicationOrderIncludesIcnMajig
-        extends AbstractIncludesIcnMajig<MedicationOrder, MedicationOrder.Entry, MedicationOrder.Bundle> {
-    /** Converts the reference to a Datamart Reference to pull out the patient id. */
-    public MedicationOrderIncludesIcnMajig() {
-        super(
-                MedicationOrder.class,
-                MedicationOrder.Bundle.class,
-                body -> Stream.ofNullable(Transformers.asReferenceId(body.patient())));
-    }
+    extends AbstractIncludesIcnMajig<
+        MedicationOrder, MedicationOrder.Entry, MedicationOrder.Bundle> {
+  /** Converts the reference to a Datamart Reference to pull out the patient id. */
+  public MedicationOrderIncludesIcnMajig() {
+    super(
+        MedicationOrder.class,
+        MedicationOrder.Bundle.class,
+        body -> Stream.ofNullable(Transformers.asReferenceId(body.patient())));
+  }
 }
--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationorder/MedicationOrderIncludesIcnMajigTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationorder/MedicationOrderIncludesIcnMajigTest.java
@@ -8,17 +8,17 @@ import org.junit.Test;
 
 public class MedicationOrderIncludesIcnMajigTest {
 
-    @Test
-    public void extractIcn() {
-        ExtractIcnValidator.<MedicationOrderIncludesIcnMajig, MedicationOrder>builder()
-                .majig(new MedicationOrderIncludesIcnMajig())
-                .body(
-                        MedicationOrder.builder()
-                                .id("123")
-                                .patient(Reference.builder().reference("Patient/1010101010V666666").build())
-                                .build())
-                .expectedIcns(List.of("1010101010V666666"))
-                .build()
-                .assertIcn();
-    }
+  @Test
+  public void extractIcn() {
+    ExtractIcnValidator.<MedicationOrderIncludesIcnMajig, MedicationOrder>builder()
+        .majig(new MedicationOrderIncludesIcnMajig())
+        .body(
+            MedicationOrder.builder()
+                .id("123")
+                .patient(Reference.builder().reference("Patient/1010101010V666666").build())
+                .build())
+        .expectedIcns(List.of("1010101010V666666"))
+        .build()
+        .assertIcn();
+  }
 }
