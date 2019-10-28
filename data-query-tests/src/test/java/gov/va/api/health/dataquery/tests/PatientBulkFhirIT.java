3b93253f5fd551ac3f4c8b1087790ca409aa8314
ilaflamme-va <43884028+ilaflamme-va@users.noreply.github.com>
2019-10-28 10:54:44 -0400

    fixing missing header in IT tests (#373)
    Fix BulkIT tests by adding the bulk header to requests

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/PatientBulkFhirIT.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/PatientBulkFhirIT.java
@@ -5,15 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.collect.ImmutableMap;
 import gov.va.api.health.argonaut.api.resources.Patient;
 import gov.va.api.health.dataquery.service.controller.BulkFhirCount;
-import gov.va.api.health.dataquery.tests.categories.LabDataQueryClinician;
 import gov.va.api.health.dataquery.tests.categories.LabDataQueryPatient;
-import gov.va.api.health.dataquery.tests.categories.ProdDataQueryClinician;
 import gov.va.api.health.dataquery.tests.categories.ProdDataQueryPatient;
 import gov.va.api.health.sentinel.ExpectedResponse;
 import gov.va.api.health.sentinel.categories.Local;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import java.util.LinkedList;
 import java.util.List;
 import lombok.SneakyThrows;
@@ -24,15 +19,8 @@ import org.junit.experimental.categories.Category;
 @Slf4j
 public class PatientBulkFhirIT {
 
-  private final ResourceVerifier verifier = ResourceVerifier.get();
-
-  private final RequestSpecification bulk =
-      RestAssured.given()
-          .spec(verifier.dataQuery().service().requestSpecification())
-          .headers(ImmutableMap.of("bulk", System.getProperty("bulk-token", "some-token")));
-
   private final String apiPath() {
-    return verifier.dataQuery().service().apiPath();
+    return ResourceVerifier.get().dataQuery().service().apiPath();
   }
 
   @Test
@@ -40,22 +28,29 @@ public class PatientBulkFhirIT {
     value = {
       Local.class,
       LabDataQueryPatient.class,
-      LabDataQueryClinician.class,
       ProdDataQueryPatient.class,
-      ProdDataQueryClinician.class
     }
   )
   public void bulkFhirPatientSearch() {
     ExpectedResponse responseAll =
-        TestClients.dataQuery().get(apiPath() + "internal/bulk/Patient?page=1&_count=6");
+        TestClients.dataQuery()
+            .get(
+                ImmutableMap.of("bulk", System.getProperty("bulk-token", "some-token")),
+                apiPath() + "internal/bulk/Patient?page=1&_count=6");
     responseAll.expect(200);
     List<Patient> allPatients = responseAll.expectListOf(Patient.class);
     ExpectedResponse responseFirstHalf =
-        TestClients.dataQuery().get(apiPath() + "internal/bulk/Patient?page=1&_count=3");
+        TestClients.dataQuery()
+            .get(
+                ImmutableMap.of("bulk", System.getProperty("bulk-token", "some-token")),
+                apiPath() + "internal/bulk/Patient?page=1&_count=3");
     responseFirstHalf.expect(200);
     List<Patient> firstHalfPatients = responseFirstHalf.expectListOf(Patient.class);
     ExpectedResponse responseSecondHalf =
-        TestClients.dataQuery().get(apiPath() + "internal/bulk/Patient?page=2&_count=3");
+        TestClients.dataQuery()
+            .get(
+                ImmutableMap.of("bulk", System.getProperty("bulk-token", "some-token")),
+                apiPath() + "internal/bulk/Patient?page=2&_count=3");
     responseSecondHalf.expect(200);
     List<Patient> secondHalfPatients = responseSecondHalf.expectListOf(Patient.class);
     List<Patient> combined = new LinkedList<>();
@@ -70,20 +65,13 @@ public class PatientBulkFhirIT {
   @Category({Local.class, LabDataQueryPatient.class, ProdDataQueryPatient.class})
   @SneakyThrows
   public void bulkPatientCount() {
-    verifyBulkFhirCount("Patient");
-  }
-
-  /**
-   * Ensures the response is valid and the count is non-trivial. The count can change between
-   * environments or if records were added. Little is gained from checking for a larger number of
-   * records.
-   */
-  public void verifyBulkFhirCount(String resource) {
-    String path = apiPath() + "internal/bulk/" + resource + "/count";
-    log.info("Verify count for {} is valid", path);
-    Response response = bulk.get(path);
-    assertThat(response.getStatusCode()).isEqualTo(200);
-    BulkFhirCount bulkFhirCount = response.as(BulkFhirCount.class);
+    ExpectedResponse response =
+        TestClients.dataQuery()
+            .get(
+                ImmutableMap.of("bulk", System.getProperty("bulk-token", "some-token")),
+                apiPath() + "internal/bulk/Patient/count");
+    response.expect(200);
+    var bulkFhirCount = response.expectValid(BulkFhirCount.class);
     assertThat(bulkFhirCount.count()).isGreaterThan(3);
   }
 }
